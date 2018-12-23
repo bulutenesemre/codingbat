5490aee2d2b740fd7761fdf3fe4b9c6d041d91ea
Enes Emre BULUT <bulute@turcom.com.tr>
2018-12-23 13:49:34 -0800

    Logic-2 added

---
+++ Logic-2/lucky_sum.py
@@ -0,0 +1,12 @@
+'''Given 3 int values, a b c, return their sum. 
+However, if one of the values is 13 then it does not count towards the sum and values to its right do not count. 
+So for example, if b is 13, then both b and c do not count.'''
+
+def lucky_sum(a, b, c):
+  total=0
+  for i in (a,b,c):
+    if i !=13:
+      total+=i
+    else:
+      break
+  return total
---
+++ Logic-2/make_chocolate.py
@@ -0,0 +1,12 @@
+'''We want make a package of goal kilos of chocolate. We have small bars (1 kilo each) and big bars (5 kilos each). 
+Return the number of small bars to use, assuming we always use big bars before small bars. Return -1 if it can't be done.'''
+
+def make_chocolate(small, big, goal):
+  if goal>=5*big:
+    last=goal-5*big
+  else:
+    last=goal%5
+
+  if last<=small:
+    return last
+  return -1
